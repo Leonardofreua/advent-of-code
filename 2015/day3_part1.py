14610b31b96dde7153a552a0b8e59a69236f83e8
Leonardo Freua <leonardo.batista.freua@gmail.com>
2022-05-28 22:08:16 -0300

    2015: Day 3
    Part 1

---
+++ 2015/day3_part1.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+# Source: https://adventofcode.com/2015/day/3s
+
+
+import os
+
+COORDINATES = {
+    "^": (0, 1),
+    "v": (0, -1),
+    "<": (-1, 0),
+    ">": (1, 0),
+}
+WIDTH = HEIGHT = 1000
+
+x = y = 500
+matrix = [[0 for _ in range(WIDTH)] for _ in range(HEIGHT)]
+matrix[x][y] = 1
+
+with open(f"{os.getcwd()}/2015/inputs/day3_input.txt") as file:
+    for direction in file.read():
+        x_coord, y_coord = COORDINATES[direction]
+
+        x += x_coord
+        y += y_coord
+
+        matrix[x][y] += 1
+
+count = 0
+for paths in matrix:
+    count += len(list(filter(lambda n: n >= 1, paths)))
+
+print(f"Number of houses that received at least one present: {count}")
