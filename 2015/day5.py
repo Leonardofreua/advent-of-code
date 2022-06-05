5545d8d52d63b4569a9e7cb033d10c1c6d4bf5e0
Leonardo Freua <leonardo.batista.freua@gmail.com>
2022-06-04 23:21:10 -0300

    2015: Day 5
    Part 1

---
+++ 2015/day5.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+# Source: https://adventofcode.com/2015/day/5
+
+VOWELS = "aeiou"
+UNWANTED_STRINGS = ["ab", "cd", "pq", "xy"]
+
+
+def has_at_least_three_vowels(string):
+    result = [char for char in string if char in VOWELS]
+    return len(result) >= 3
+
+
+def contains_at_least_one_letter_equal_side_by_side(string):
+    for index in range(1, len(string)):
+        if string[index] == string[index - 1]:
+            return True
+    return False
+
+
+def has_not_unwanted_string(string):
+    return not any(value for value in UNWANTED_STRINGS if value in string)
+
+
+with open("./inputs/day5_input.txt") as file:
+    nice_strings = 0
+    for line in file:
+        string = line.strip()
+        if (
+            has_at_least_three_vowels(string)
+            and contains_at_least_one_letter_equal_side_by_side(string)
+            and has_not_unwanted_string(string)
+        ):
+            nice_strings += 1
+
+    print(f"Total Nice strings: {nice_strings}")
