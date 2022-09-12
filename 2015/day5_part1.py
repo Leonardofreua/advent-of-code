19a3f19b95e757c60a662c0aa77839d901593c9b
Leonardo Freua <leonardo.batista.freua@gmail.com>
2022-09-11 23:51:54 -0300

    Add data typing in day5_part1 file functions.

--- 2015/day5_part1.py
+++ 2015/day5_part1.py
@@ -6,19 +6,19 @@ VOWELS = "aeiou"
 UNWANTED_STRINGS = ["ab", "cd", "pq", "xy"]
 
 
-def has_at_least_three_vowels(string):
+def has_at_least_three_vowels(string: str) -> bool:
     result = [char for char in string if char in VOWELS]
     return len(result) >= 3
 
 
-def contains_at_least_one_letter_equal_side_by_side(string):
+def contains_at_least_one_letter_equal_side_by_side(string: str) -> bool:
     for index in range(1, len(string)):
         if string[index] == string[index - 1]:
             return True
     return False
 
 
-def has_not_unwanted_string(string):
+def has_not_unwanted_string(string: str) -> bool:
     return not any(value for value in UNWANTED_STRINGS if value in string)
 
 
